6b73895d4753be2a0e9a6ed0ebb97798b2e2e982
Chandara Sin <>
2022-09-03 14:01:33 +0700

    refactor: add graceful-shutdown

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"os/signal"
 	"slack-clone-api/config"
 	"slack-clone-api/domain/user"
 	"slack-clone-api/store"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -24,5 +31,31 @@ func main() {
 	u := r.Group("/api")
 	u.POST("/users", user.CreateUserHanlder(user.Create(db)))
 
-	r.Run()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + viper.GetString("app.port"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }
