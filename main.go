97fbaa9ad186690eeec7c3f8afbcede309ced6d5
Chandara Sin <cdsin68@gmail.com>
2022-09-03 13:55:33 +0700

    refactor: move init config to config directory

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"slack-clone-api/config"
 	"slack-clone-api/domain/user"
 	"slack-clone-api/store"
 
-	"strings"
-
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	initConfig()
+	config.InitConfig()
 	r := gin.Default()
 	r.GET("/healthz", func(c *gin.Context) {
 		c.Status(200)
@@ -28,16 +25,4 @@ func main() {
 	u.POST("/users", user.CreateUserHanlder(user.Create(db)))
 
 	r.Run()
-}
-
-func initConfig() {
-	viper.SetConfigName("config-dev") // name of config file (without extension)
-	viper.SetConfigType("yaml")       // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")          // optionally look for config in the working directory
-	err := viper.ReadInConfig()       // Find and read the config file
-	if err != nil {
-		fmt.Printf("Fatal error config file: %s \n", err) // Handle errors reading the config file
-	}
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
 }
